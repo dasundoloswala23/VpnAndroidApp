b897ef41274dd36974520be02cba9b2536e360c6
dasun <tharindudasun0@gmail.com>
2022-08-09 12:40:57 +0530

    vpn app

---
+++ app/build/generated/source/buildConfig/release/com/lunar/bestVPN/BuildConfig.java
@@ -0,0 +1,24 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.lunar.bestVPN;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.lunar.bestVPN";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "2.1";
+  // Field from default config.
+  public static final String BASE_CARRIER_ID = "Sof_airvpn";
+  // Field from default config.
+  public static final String BASE_HOST = "https://d2isj403unfbyl.cloudfront.net";
+  // Field from default config.
+  public static final String BASE_OAUTH_METHOD = "anonymous";
+  // Field from default config.
+  public static final String SHARED_PREFS = "NORTHGHOST_SHAREDPREFS";
+  // Field from default config.
+  public static final String STORED_CARRIER_ID_KEY = "com.northghost.afvclient.CARRIER_ID_KEY";
+  // Field from default config.
+  public static final String STORED_HOST_URL_KEY = "com.northghost.afvclient.STORED_HOST_KEY";
+}
